f379e5fccd26d9a8e3723bb5f435f1b92f8b297c
Beryl-18 <beryla.s@hotmail.com>
2023-07-19 12:07:17 +0400

    setting up schemas for the sanity cms

--- instame_backend/schemas/index.js
+++ instame_backend/schemas/index.js
@@ -1,11 +1,3 @@
 import user from './user';
 
-
-// export default createSchema({
-//     name:'default',
-//     types: schemaTypes.concat([
-//         user
-//     ])
-// })
-
 export const schemaTypes = [user]
